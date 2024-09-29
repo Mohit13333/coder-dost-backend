650ef7a06c94962cc5b5e625bd3efd639933cfda
Mohit13333 <mohitkumar143680@gmail.com>
2024-09-29 16:24:18 +0530

    routes

--- controller/controllers/products.js
+++ controller/controllers/products.js
@@ -1,41 +1,43 @@
 import fs from "fs";
-const fs = require('fs');
 // const index = fs.readFileSync('index.html', 'utf-8');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 const products = data.products;
 
-ecreateProduct = (req, res) => {
+const createProduct = (req, res) => {
   console.log(req.body);
   products.push(req.body);
   res.status(201).json(req.body);
 };
 
-exports.getAllProducts = (req, res) => {
+const getAllProducts = (req, res) => {
   res.json(products);
 };
 
-exports.getProduct = (req, res) => {
+const getProduct = (req, res) => {
   const id = +req.params.id;
   const product = products.find((p) => p.id === id);
   res.json(product);
 };
-exports.replaceProduct = (req, res) => {
+const replaceProduct = (req, res) => {
   const id = +req.params.id;
   const productIndex = products.findIndex((p) => p.id === id);
   products.splice(productIndex, 1, { ...req.body, id: id });
   res.status(201).json();
 };
-exports.updateProduct = (req, res) => {
+const updateProduct = (req, res) => {
   const id = +req.params.id;
   const productIndex = products.findIndex((p) => p.id === id);
   const product = products[productIndex];
   products.splice(productIndex, 1, { ...product, ...req.body });
   res.status(201).json();
 };
-exports.deleteProduct = (req, res) => {
+const deleteProduct = (req, res) => {
   const id = +req.params.id;
   const productIndex = products.findIndex((p) => p.id === id);
   const product = products[productIndex];
   products.splice(productIndex, 1);
   res.status(201).json(product);
 };
+
+
+export {createProduct,getAllProducts,getProduct,replaceProduct,updateProduct,deleteProduct}
