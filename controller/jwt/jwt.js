eeedd888b1425d194ef5a956765caa151af52268
MOHIT SINGH <92773869+Mohit13333@users.noreply.github.com>
2024-10-12 14:25:29 +0530

    Update jwt.js

--- controller/jwt/jwt.js
+++ controller/jwt/jwt.js
@@ -1,24 +1,24 @@
-import jwt from "jsonwebtoken";
-import path from "path";
-import fs from "fs";
+// import jwt from "jsonwebtoken";
+// import path from "path";
+// import fs from "fs";
 
 
-const __dirname = path.dirname('conroller');
-const publicekey=fs.readFileSync(path.join(__dirname,"public.key"),"utf-8");
-const auth = (req, res, next) => {
-  try {
-    const token = req.get("authorization").split("Bearer ")[1];
-    console.log(token);
-    let decoded = jwt.verify(token, publicekey);
-    console.log(decoded);
-    if (decoded.email) {
-      next();
-    } else {
-      res.sendStatus(401);
-    }
-  } catch (error) {
-    res.sendStatus(401);
-  }
-};
+// const __dirname = path.dirname('conroller');
+// const publicekey=fs.readFileSync(path.join(__dirname,"public.key"),"utf-8");
+// const auth = (req, res, next) => {
+//   try {
+//     const token = req.get("authorization").split("Bearer ")[1];
+//     console.log(token);
+//     let decoded = jwt.verify(token, publicekey);
+//     console.log(decoded);
+//     if (decoded.email) {
+//       next();
+//     } else {
+//       res.sendStatus(401);
+//     }
+//   } catch (error) {
+//     res.sendStatus(401);
+//   }
+// };
 
-export default auth;
+// export default auth;
